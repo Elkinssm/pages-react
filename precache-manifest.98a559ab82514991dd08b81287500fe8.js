85a97cd1f00163b1cd512ec4fd336e0bc7084f91
elkinssm <elkinsilvamanrique@gmail.com>
2019-11-18 10:31:22 -0500

    Updates

---
+++ precache-manifest.98a559ab82514991dd08b81287500fe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c06db298d7887b813c3c0ef8334b202",
+    "url": "/pages-react/index.html"
+  },
+  {
+    "revision": "46b72a911c06b13721b2",
+    "url": "/pages-react/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "396b4fb83109436c0f8d",
+    "url": "/pages-react/static/css/main.7a4e0258.chunk.css"
+  },
+  {
+    "revision": "46b72a911c06b13721b2",
+    "url": "/pages-react/static/js/2.9d8f1f48.chunk.js"
+  },
+  {
+    "revision": "396b4fb83109436c0f8d",
+    "url": "/pages-react/static/js/main.384aea24.chunk.js"
+  },
+  {
+    "revision": "661b5dec7d2d5176550a",
+    "url": "/pages-react/static/js/runtime-main.22cc2d88.js"
+  }
+]);
